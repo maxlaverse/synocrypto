c58c5b2c16dd90c26953abf61ab0049540a6aa3b
Max Laverse <max@laverse.net>
2022-09-16 20:55:20 +0200

    add pkcsPad and extract pkcs*

--- pkg/crypto/decrypter.go
+++ pkg/crypto/decrypter.go
@@ -172,27 +172,3 @@ func (d *decrypter) Close() error {
 	}
 	return err
 }
-
-// Returns slice of the original data without padding.
-// https://golang-examples.tumblr.com/post/98350728789/pkcs-7-padding
-func pkcs7Unpad(data []byte, blocklen int) ([]byte, error) {
-	if blocklen <= 0 {
-		return nil, fmt.Errorf("invalid blocklen %d", blocklen)
-	}
-	if len(data)%blocklen != 0 || len(data) == 0 {
-		return nil, fmt.Errorf("invalid data len %d", len(data))
-	}
-	padlen := int(data[len(data)-1])
-	if padlen > blocklen || padlen == 0 {
-		return nil, fmt.Errorf("invalid padding")
-	}
-	// check padding
-	pad := data[len(data)-padlen:]
-	for i := 0; i < padlen; i++ {
-		if pad[i] != byte(padlen) {
-			return nil, fmt.Errorf("invalid padding")
-		}
-	}
-
-	return data[:len(data)-padlen], nil
-}
---
+++ pkg/crypto/pkcs.go
@@ -0,0 +1,45 @@
+package crypto
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// From  https://golang-examples.tumblr.com/post/98350728789/pkcs-7-padding
+
+// Returns slice of the original data without padding.
+func pkcs7Unpad(data []byte, blocklen int) ([]byte, error) {
+	if blocklen <= 0 {
+		return nil, fmt.Errorf("invalid blocklen %d", blocklen)
+	}
+	if len(data)%blocklen != 0 || len(data) == 0 {
+		return nil, fmt.Errorf("invalid data len %d", len(data))
+	}
+	padlen := int(data[len(data)-1])
+	if padlen > blocklen || padlen == 0 {
+		return nil, fmt.Errorf("invalid padding (padlen: %d, blocklen: %d)", padlen, blocklen)
+	}
+	// check padding
+	pad := data[len(data)-padlen:]
+	for i := 0; i < padlen; i++ {
+		if pad[i] != byte(padlen) {
+			return nil, fmt.Errorf("invalid padding (padlen: %d, pad: %d)", padlen, pad)
+		}
+	}
+
+	return data[:len(data)-padlen], nil
+}
+
+// Returns slice of data with padding.
+func pkcs7Pad(data []byte, blocklen int) ([]byte, error) {
+	if blocklen <= 0 {
+		return nil, fmt.Errorf("invalid blocklen %d", blocklen)
+	}
+	padlen := 1
+	for ((len(data) + padlen) % blocklen) != 0 {
+		padlen = padlen + 1
+	}
+
+	pad := bytes.Repeat([]byte{byte(padlen)}, padlen)
+	return append(data, pad...), nil
+}
