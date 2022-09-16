078b56d7c504ee269798a818a74c0882fdbdc9e1
Max Laverse <max@laverse.net>
2022-09-16 20:57:42 +0200

    add encrypter

---
+++ pkg/crypto/encrypter.go
@@ -0,0 +1,188 @@
+package crypto
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io"
+)
+
+const (
+	// maxBlockSize is the maximum size an object can have. If the limit
+	// is cross, CloudSync complains about it and says it considers the file
+	// to be corrupted.
+	maxBlockSize = 4096
+)
+
+// DecryptOnceWithPasswordAndSalt decrypts a single blob of data with
+// an AES decrypter initialized by password and salt
+func EncryptOnceWithPasswordAndSalt(password, salt, decodedData []byte) ([]byte, error) {
+	var b bytes.Buffer
+	sessionKeyEncrypter := NewEncrypterWithPasswordAndSalt(password, salt, &b)
+
+	_, err := sessionKeyEncrypter.Write(decodedData)
+	if err != nil {
+		return nil, fmt.Errorf("error decrypting encrypted key1: '%w'", err)
+	}
+
+	err = sessionKeyEncrypter.Close()
+	if err != nil {
+		return nil, fmt.Errorf("error decrypting encrypted key1: '%w'", err)
+	}
+
+	return b.Bytes(), nil
+}
+
+// EncryptOnceWithPublicKey encrypts a single blob of data using an RSA public key
+func EncryptOnceWithPublicKey(publicKey []byte, decodedData []byte) ([]byte, error) {
+	publicKeyBlock, _ := pem.Decode(publicKey)
+	if publicKeyBlock == nil {
+		return nil, fmt.Errorf("no public key found in pem")
+	}
+
+	key, err := x509.ParsePKCS1PublicKey(publicKeyBlock.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse public key: %w", err)
+	}
+
+	encryptedData, err := encryptOAEP(key, decodedData)
+	if err != nil {
+		return nil, fmt.Errorf("could not encrypt data using public key: %w", err)
+	}
+
+	return encryptedData, nil
+}
+
+func encryptOAEP(public *rsa.PublicKey, p []byte) ([]byte, error) {
+	hash := sha1.New()
+	random := rand.Reader
+
+	step := public.Size() - 2*hash.Size() - 2
+
+	var outputEncrypted bytes.Buffer
+	for start := 0; start < len(p); start += step {
+		end := start + step
+		if end > len(p) {
+			end = len(p)
+		}
+		encryptedBlockBytes, err := rsa.EncryptOAEP(hash, random, public, p[start:end], nil)
+		if err != nil {
+			return nil, err
+		}
+		outputEncrypted.Write(encryptedBlockBytes)
+	}
+
+	return outputEncrypted.Bytes(), nil
+}
+
+func NewEncrypterWithPasswordAndSalt(password, salt []byte, out io.Writer) io.WriteCloser {
+	key, iv := keyIV(password, salt)
+	return newAESCBCEncrypter(key, iv, out)
+}
+
+func newAESCBCEncrypter(key, iv []byte, out io.Writer) io.WriteCloser {
+	block, _ := aes.NewCipher(key)
+	return &encrypter{
+		mode: cipher.NewCBCEncrypter(block, iv),
+		out:  out,
+	}
+}
+
+type encrypter struct {
+	hasBufferedBlock bool
+	mode             cipher.BlockMode
+	bufferedBlock    []byte
+	out              io.Writer
+}
+
+func (d *encrypter) Write(p []byte) (int, error) {
+	if len(p) == 0 {
+		return d.write(nil)
+	}
+
+	written := 0
+	for start := 0; start < len(p); start += maxBlockSize {
+		end := start + maxBlockSize
+		if end > len(p) {
+			end = len(p)
+		}
+
+		n, err := d.write(p[start:end])
+		written = written + n
+		if err != nil {
+			return written, err
+		}
+	}
+	return written, nil
+}
+
+func (d *encrypter) write(p []byte) (int, error) {
+	if d.hasBufferedBlock {
+		lastBlockEncrypted := make([]byte, len(d.bufferedBlock))
+
+		err := func() (err error) {
+			defer func() {
+				if r := recover(); r != nil {
+					err = fmt.Errorf("CryptBlocks paniced: %v", r)
+				}
+			}()
+
+			d.mode.CryptBlocks(lastBlockEncrypted, d.bufferedBlock)
+			return
+		}()
+		if err != nil {
+			return -1, fmt.Errorf("error crypting block: %w", err)
+		}
+
+		n, err := d.out.Write(lastBlockEncrypted)
+		if err != nil {
+			return n, fmt.Errorf("error flushing block: %w", err)
+		}
+		d.hasBufferedBlock = false
+	}
+
+	// We have to return len(p) instead of what was really written
+	// to the output, or io.Copy() returns 'short write' error.
+	if len(p) == 0 {
+		return len(p), nil
+	}
+
+	d.bufferedBlock = make([]byte, len(p))
+	n := copy(d.bufferedBlock, p)
+	if n != len(p) {
+		panic(fmt.Sprintf("%d/%d/%d", n, len(p), len(d.bufferedBlock)))
+	}
+	d.hasBufferedBlock = true
+	return len(p), nil
+}
+
+func (d *encrypter) flushBufferWithPadding() (int, error) {
+	if !d.hasBufferedBlock {
+		return -1, nil
+	}
+
+	var err error
+	d.bufferedBlock, err = pkcs7Pad(d.bufferedBlock, d.mode.BlockSize())
+	if err != nil {
+		return -1, fmt.Errorf("unable to pad data: %w", err)
+	}
+
+	return d.Write(nil)
+}
+
+func (d *encrypter) Close() error {
+	_, err := d.flushBufferWithPadding()
+
+	if v, ok := d.out.(io.WriteCloser); ok {
+		if err := v.Close(); err != nil {
+			return err
+		}
+	}
+	return err
+}
---
+++ pkg/crypto/encrypter_test.go
@@ -0,0 +1,68 @@
+package crypto
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/maxlaverse/synocrypto/testdata"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncrypt(t *testing.T) {
+	testCases := []struct {
+		givenSessionKey []byte
+		givenData       string
+		expectedBytes   []byte
+	}{
+		{
+			[]byte{54, 67, 49, 70, 68, 52, 70, 65, 57, 53, 54, 54, 48, 52, 56, 65, 67, 69, 53, 55, 66, 69, 56, 53, 70, 67, 54, 48, 48, 69, 68, 57, 49, 52, 55, 57, 57, 70, 50, 65, 49, 65, 68, 51, 49, 50, 49, 50, 68, 54, 54, 55, 56, 68, 51, 48, 65, 67, 48, 49, 53, 68, 50, 50},
+			"coucou",
+			[]byte{0x1e, 0x31, 0xdd, 0x53, 0xe, 0xb, 0x59, 0x6d, 0x21, 0x1d, 0xd6, 0x40, 0xad, 0x7b, 0x1c, 0x9f},
+		},
+		{
+			[]byte{54, 67, 49, 70, 68, 52, 70, 65, 57, 53, 54, 54, 48, 52, 56, 65, 67, 69, 53, 55, 66, 69, 56, 53, 70, 67, 54, 48, 48, 69, 68, 57, 49, 52, 55, 57, 57, 70, 50, 65, 49, 65, 68, 51, 49, 50, 49, 50, 68, 54, 54, 55, 56, 68, 51, 48, 65, 67, 48, 49, 53, 68, 50, 50},
+			"somethingthatismuchmorelonger",
+			[]byte{0x73, 0xac, 0x6, 0x48, 0xd6, 0x30, 0x1a, 0xef, 0xbd, 0xac, 0xbe, 0x8, 0x2e, 0xba, 0x16, 0x1e, 0x82, 0x48, 0x8b, 0xa9, 0xd9, 0x53, 0x25, 0x94, 0xd6, 0x2a, 0x48, 0x72, 0xcc, 0x26, 0xe2, 0x25},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			var outputEncrypted bytes.Buffer
+			encrypter := NewEncrypterWithPasswordAndSalt([]byte(tc.givenSessionKey), []byte{}, &outputEncrypted)
+			n, err := encrypter.Write([]byte(tc.givenData))
+			assert.NoError(t, err)
+			assert.Equal(t, len(tc.givenData), n)
+
+			err = encrypter.Close()
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expectedBytes, outputEncrypted.Bytes())
+
+			var outputDecrypted bytes.Buffer
+			decrypter := NewDecrypterWithPasswordAndSalt([]byte(tc.givenSessionKey), []byte{}, &outputDecrypted)
+			n, err = decrypter.Write(outputEncrypted.Bytes())
+			assert.NoError(t, err)
+			assert.Equal(t, 0, n)
+
+			err = decrypter.Close()
+			assert.NoError(t, err)
+			assert.Equal(t, tc.givenData, outputDecrypted.String())
+		})
+	}
+}
+
+func TestEncryptingWithPublicKey(t *testing.T) {
+	testCases := []string{"6C1FD4FA9566048ACE57BE85FC600ED914799F2A1AD31212D6678D30AC015D22"}
+
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			encryptedSessionKey, err := EncryptOnceWithPublicKey([]byte(testdata.FixturePublicKey), []byte(tc))
+			assert.NoError(t, err)
+
+			sessionKey, err := DecryptOnceWithPrivateKey([]byte(testdata.FixturePrivateKey), []byte(encryptedSessionKey))
+			assert.NoError(t, err)
+
+			assert.Equal(t, string(sessionKey), tc)
+		})
+	}
+}
